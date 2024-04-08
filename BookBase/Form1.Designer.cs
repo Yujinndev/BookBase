823634d5e70ab87e1ec122dec0636323fbe0930c
Yujinndev <xyujinwho@gmail.com>
2024-04-09 01:46:07 +0800

    feat: implement card view of books from database

--- BookBase/Form1.Designer.cs
+++ BookBase/Form1.Designer.cs
@@ -29,12 +29,114 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.materialTabControl1 = new MaterialSkin.Controls.MaterialTabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.materialTabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // materialTabControl1
+            // 
+            this.materialTabControl1.Controls.Add(this.tabPage1);
+            this.materialTabControl1.Controls.Add(this.tabPage2);
+            this.materialTabControl1.Depth = 0;
+            this.materialTabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.materialTabControl1.ImageList = this.imageList1;
+            this.materialTabControl1.Location = new System.Drawing.Point(3, 64);
+            this.materialTabControl1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialTabControl1.Multiline = true;
+            this.materialTabControl1.Name = "materialTabControl1";
+            this.materialTabControl1.SelectedIndex = 0;
+            this.materialTabControl1.Size = new System.Drawing.Size(1069, 658);
+            this.materialTabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.White;
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.ImageKey = "Homeee.png";
+            this.tabPage1.Location = new System.Drawing.Point(4, 39);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1061, 615);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Home";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.White;
+            this.tabPage2.Controls.Add(this.flowLayoutPanel1);
+            this.tabPage2.ImageKey = "Libraryyy.png";
+            this.tabPage2.Location = new System.Drawing.Point(4, 39);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1061, 615);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Library";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.White;
+            this.imageList1.Images.SetKeyName(0, "Libraryyy.png");
+            this.imageList1.Images.SetKeyName(1, "Homeee.png");
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1055, 609);
+            this.flowLayoutPanel1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(215, 75);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Home";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(1075, 725);
+            this.Controls.Add(this.materialTabControl1);
+            this.DrawerIsOpen = true;
+            this.DrawerShowIconsWhenHidden = true;
+            this.DrawerTabControl = this.materialTabControl1;
+            this.MinimumSize = new System.Drawing.Size(815, 600);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BookBase";
+            this.materialTabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private MaterialSkin.Controls.MaterialTabControl materialTabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- BookBase/Form1.cs
+++ BookBase/Form1.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using BookBase.Controllers;
+using BookBase.Models;
+using MaterialSkin.Controls;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
@@ -7,14 +11,95 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Net;
 
 namespace BookBase
 {
-    public partial class Form1 : Form
+    public partial class Form1 : MaterialForm
     {
+        private List<Book> books;
+        private LibraryController libraryController;
+
         public Form1()
         {
             InitializeComponent();
+            libraryController = new LibraryController(); // Instantiate LibraryController
+            books = libraryController.GetAllBooks(); // Call GetAllBooks to get the list of books
+            DisplayProducts(flowLayoutPanel1);
+        }
+
+        public void DisplayProducts(FlowLayoutPanel container)
+        {
+            foreach (Book book in books)
+            {
+                MaterialCard cardPanel = new MaterialCard
+                {
+                    Size = new Size(225, 300),
+                    Margin = new Padding(10),
+                };
+
+                PictureBox pictureBox = new PictureBox
+                {
+                    SizeMode = PictureBoxSizeMode.StretchImage,
+                    Dock = DockStyle.Top,
+                    Height = 175
+                };
+
+                ImageLoad(pictureBox, book);
+
+                Label titleLabel = new Label
+                {
+                    Text = book.title,
+                    AutoSize = false,
+                    Dock = DockStyle.Top,
+                    TextAlign = ContentAlignment.MiddleCenter,
+                    Font = new Font("Arial", 12, FontStyle.Bold),
+                    Height = 25
+                };
+
+                MaterialLabel authorLabel = new MaterialLabel
+                {
+                    Text = book.author,
+                    AutoSize = false,
+                    Dock = DockStyle.Top,
+                    TextAlign = ContentAlignment.MiddleCenter,
+                    Height = 25
+                };
+
+                MaterialButton addToCartButton = new MaterialButton
+                {
+                    Text = "Borrow",
+                    Dock = DockStyle.Bottom
+                };
+
+                cardPanel.Controls.Add(addToCartButton);
+                cardPanel.Controls.Add(authorLabel);
+                cardPanel.Controls.Add(titleLabel);
+                cardPanel.Controls.Add(pictureBox);
+
+                container.Controls.Add(cardPanel);
+            }
+        }
+        
+        public async void ImageLoad(PictureBox pictureBox, Book book)
+        {
+            using (WebClient client = new WebClient())
+            {
+                client.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3");
+                try
+                {
+                    byte[] imageData = await client.DownloadDataTaskAsync(book.image_url);
+                    using (MemoryStream stream = new MemoryStream(imageData))
+                    {
+                        pictureBox.Image = Image.FromStream(stream);
+                    }
+                }
+                catch (WebException ex)
+                {
+                    MessageBox.Show($"Error loading image for {book.title}: {ex.Message}");
+                }
+            }
         }
     }
 }
